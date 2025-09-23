292f01ea013248b9a57ab8ef5c6e0413bdd7bc1e
binhbinh214 <binhhu21974@gmail.com>
2025-09-23 22:19:59 +0700

    add status active to getBookbySlug

--- src/controller/Users/book.controller.js
+++ src/controller/Users/book.controller.js
@@ -5,6 +5,7 @@ exports.getBookBySlug = async (req, res) => {
     const book = await Book.findOne({
       slug: req.params.slug,
       deleted: false,
+      status: "active",
     }).populate("category_id");
     if (!book) return res.status(404).json({ message: "Book not found" });
     res.json(book);
