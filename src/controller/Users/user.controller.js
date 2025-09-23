b7d10c36952369bc1aa64dc6bd4da6627b9b820b
Manh <manhselu@gmail.com>
2025-09-23 23:03:14 +0700

    Cập nhật thêm search theo keyword(tên) cho Hàm phân trang + filter filterProductPaginated

--- src/controller/Users/user.controller.js
+++ src/controller/Users/user.controller.js
@@ -1,41 +1,44 @@
 const Product = require("../../model/Book");
 const Category = require("../../model/Category");
 
-//Hàm search Book
-exports.searchProduct = async (req, res) => {
+// Hàm Search và Filter Book theo category
+exports.findAndFilterProductPaginated = async (req, res) => {
   try {
-    const { keyword } = req.body; 
+    const { categoryTitle = "",keyword = "", page = 1 } = req.query;
+    const pageSize = 5;
+    const skip = (page - 1) * pageSize; // ==> Bỏ qua sản phẩm để phân trang,Ví dụ: page = 2, limit = 5 → skip = 5
+    // → bỏ 5 sản phẩm đầu, lấy sản phẩm từ thứ 6 trở đi.
+    // Hàm lấy tất cả Product
+    const allProductsQuery = { status: "active" };
+    let allProducts = await Product.find(allProductsQuery).populate(
+      "categori_id",
+      "title"
+    );
+    //Hàm search product theo keyword
     const query = { title: { $regex: keyword, $options: "i" },status : "active"}
-    const products = await Product.find(query);
-    res.json(products);
+    allProducts = await Product.find(query);
+
+    if (categoryTitle) {
+      const categoryQuery = { title: categoryTitle };
+      const category = await Category.findOne(categoryQuery);
+      if (!category) {
+        return res.status(404).json({ message: "Category not found" });
+      }
+    allProducts = allProducts.filter(p => // ==> Sau sửa lại 
+  p.categori_id.some(cat => String(cat._id) === String(category._id))
+);
+    }
+    const paginatedProducts = allProducts.slice(skip, skip + pageSize);
+    const totalItems = allProducts.length;
+    const totalPages = Math.ceil(totalItems / pageSize); // Tính tổng số page dựa trên sản phẩm đã tính
+    res.json({
+      page: page,  //trang hiện tại
+      pageSize,   // số sản phẩm/trang
+      totalItems, // tổng sản phẩm
+      totalPages, // tổng page
+      data: paginatedProducts, // danh sách sản phẩm phân trang
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
-
-// Hàm Filter Book theo category
-exports.filterProductPaginated = async(req,res) =>{
-  try {
-    const {categoryTitle, page =1} = req.body;
-    const pageSize = 5;
-    const skip = (page - 1) * pageSize; // ==> Bỏ qua sản phẩm để phân trang,Ví dụ: page = 2, limit = 5 → skip = 5 
-                                              // → bỏ 5 sản phẩm đầu, lấy sản phẩm từ thứ 6 trở đi.
-    const categoryQuery = {title : categoryTitle};
-    const category = await Category.findOne(categoryQuery);
-      if (!category) {
-      return res.status(404).json({ message: "Category not found" });
-    };
-    const query = {categori_id: category._id,status : "active"};
-    const products = await Product.find(query).skip(skip).limit(pageSize);
-    const totalItems = await Product.countDocuments(query);
-    const totalPages = Math.ceil(totalItems/pageSize); // Tính tổng số page dựa trên sản phẩm đã tính
-   res.json({
-      page: page,   // trang hiện tại
-      pageSize: pageSize,    // số sản phẩm/trang
-      totalItems,         // tổng sản phẩm
-      totalPages,         // tổng page
-      data: products      // danh sách sản phẩm phân trang
-    });  } catch (err) {
-    res.status(500).json({message: err.message});
-  }
-}
--- src/router/users/users.notcheck.routes.js
+++ src/router/users/users.notcheck.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router  = express.Router();
 const productController = require('../../controller/Users/user.controller')
 
-router.get("/search", productController.searchProduct);
-router.get("/filterPaginated", productController.filterProductPaginated);
+// router.get("/search", productController.searchProduct);
+router.get("/filterPaginated", productController.findAndFilterProductPaginated);
 module.exports = router;
  
