c264aba0a6646b2f23b6c08a679d44bd08e627ec
son090103 <daoson090103@gmail.com>
2025-09-24 22:53:07 +0700

    update

--- Server.js
+++ Server.js
@@ -6,8 +6,9 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const adminNotCheck = require("./src/router/Admin/index.notcheck.routes");
-
+const userNotCheck = require("./src/router/users/index.notcheck.routes");
 adminNotCheck(app);
+userNotCheck(app);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
