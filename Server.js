ea2bdd5a96e0db4398722dc0d6065b70a2ff1bdf
son090103 <daoson090103@gmail.com>
2025-09-24 23:12:25 +0700

    update

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const database = require("./src/config/database");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,6 +10,7 @@ const adminRouterNotCheck = require("./src/router/Admin/index.notcheck.routes");
 const userRouterNotCheck = require("./src/router/users/index.notcheck.routes");
 adminRouterNotCheck(app);
 userRouterNotCheck(app);
+database.connect();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- src/router/Admin/admin.notcheck.routes.js
+++ src/router/Admin/admin.notcheck.routes.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./../../controller/Admin/admin.controller");
-const bookController = require("../../controller/Users/book.controller");
 router.post("/loginAdmin", controller.login);
-router.post("/register", authController.register);
-router.get("/books/:slug", bookController.getBookBySlug);
 module.exports = router;
