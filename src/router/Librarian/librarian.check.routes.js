fe4cbf3105694839fe7881e49db959fffae95575
Manh <manhselu@gmail.com>
2025-10-07 21:00:16 +0700

    CRUD Book

--- src/router/Librarian/librarian.check.routes.js
+++ src/router/Librarian/librarian.check.routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const routerLibCheck = express.Router();
 const LibrarianController = require('../../controller/Librarian/librarian.controller');
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
 
 routerLibCheck.patch("/returnBook",LibrarianController.returnBorrowBook);
-
+routerLibCheck.post("/addNewBooks",upload.array("images"),LibrarianController.AddNewBooks);
+routerLibCheck.patch("/updateBooks/:bookIdInput",LibrarianController.UpdateBooks);
+routerLibCheck.delete("/deleteBooks/:bookIdInput",LibrarianController.DeleteBook);
+routerLibCheck.get("/getAllBooks",LibrarianController.GetAllBook);
 module.exports = routerLibCheck;
